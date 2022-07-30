db157bbf6444a996859ad65baaf505100b4db63f
SankaranarayananMurugan <m.sankaranarayanan@outlook.com>
2022-07-30 13:52:26 +0530

    Basic Authentication Revisited

--- 21. Basic Authentication Revisited/src/main/java/com/thecodefacts/spring/security/config/ApiSecurityConfig.java
+++ 21. Basic Authentication Revisited/src/main/java/com/thecodefacts/spring/security/config/ApiSecurityConfig.java
@@ -3,6 +3,7 @@ package com.thecodefacts.spring.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static com.thecodefacts.spring.security.constant.SecurityConstants.PUBLIC_API_LIST;
@@ -14,10 +15,10 @@ public class ApiSecurityConfig {
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                /*.sessionManagement(
+                .sessionManagement(
                         httpSecuritySessionManagementConfigurer ->
                                 httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )*/
+                )
                 .authorizeRequests(auth -> auth
                         .antMatchers(GET, PUBLIC_API_LIST).permitAll()
                         .anyRequest().authenticated()
