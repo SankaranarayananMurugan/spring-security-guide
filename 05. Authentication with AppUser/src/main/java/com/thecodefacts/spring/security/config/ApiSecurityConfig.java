1151bac515da4a85d823b231b72459f4f9310e8a
SankaranarayananMurugan <m.sankaranarayanan@outlook.com>
2022-08-14 20:14:05 +0530

    Moved 5/6 chapters ahead to 4/5 respectively

--- 05. Authentication with AppUser/src/main/java/com/thecodefacts/spring/security/config/ApiSecurityConfig.java
+++ 05. Authentication with AppUser/src/main/java/com/thecodefacts/spring/security/config/ApiSecurityConfig.java
@@ -5,16 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static com.thecodefacts.spring.security.constant.SecurityConstants.*;
-import static org.springframework.http.HttpMethod.GET;
-
 @Configuration
 public class ApiSecurityConfig {
     @Bean
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(auth -> auth
-                        .antMatchers(GET, PUBLIC_API_LIST).permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic();
